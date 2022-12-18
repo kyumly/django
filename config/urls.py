45da46d2917b482ddba0f7a09685ba98bd34db5e
kyumly <skkim3360@naver.com>
2022-12-18 22:02:20 +0900

    2-03, 04 , 장고 관리자, 조회와 템플릿 완료

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pybo/', views.index)
+    path('pybo/', include('pybo.urls')),
 ]
